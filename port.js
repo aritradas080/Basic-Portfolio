f78b6592409db7a059f1b28156bd44063b1fb92f
Aritra Das <aritradas080@gmail.com>
2023-08-21 19:13:02 +0600

    portfolio-basic
    portfolio-basic

---
+++ port.js
@@ -0,0 +1,26 @@
+const slideshowElements = document.querySelectorAll(".slideshow-element");
+console.log(slideshowElements);
+let count = 1;
+
+
+
+    setInterval(()=> {
+        count++;
+         let currentElement = document.querySelector(".current");
+    
+       
+        currentElement.classList.remove("current");
+       
+    
+        if(count>slideshowElements.length){
+            slideshowElements[0].classList.add("current");
+            count=1;
+         }else{
+            currentElement.nextElementSibling.classList.add("current");
+         }
+    },4000);
+
+
+
+
+
